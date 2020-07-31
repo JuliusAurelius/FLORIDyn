9e1f82cbedaf053e15cf6cceaea37b033a1c89f9
M. Becker <sucram.becker@gmx.de>
2020-07-31 19:11:35 +0200

    Cleaning and prep for merge

--- main/WakeModel/getBastankhahVars.m
+++ main/WakeModel/getBastankhahVars.m
@@ -20,26 +20,36 @@ function [sig_y, sig_z, C_T, Theta, k_y, k_z, x_0] = getBastankhahVars(op_dw, op
 %% Calc C_T
 a = op_ayaw(:,1);
 yaw = op_ayaw(:,2);
-%C_T = 4*a.*sqrt(1-a.*(2*cos(yaw)-a));   % [1] Eq.6.1 (creates img values for r)
-C_T = 4*a.*(1-a.*cos(yaw));             % [1] Eq.6.2
+
+% Could be replaced by look-up-table
+% [1] Eq.6.1 
+%C_T = 4*a.*sqrt(1-a.*(2*cos(yaw)-a));
+% [1] Eq.6.2
+C_T = 4*a.*(1-a.*cos(yaw));
 %% Calc x_0 (Core length)
 alpha = 2.32;
 beta = 0.154;
+% [1] Eq. 7.3
 x_0 = (cos(yaw).*(1+sqrt(1-C_T))./...
-    (sqrt(2)*(alpha*op_I+beta*(1-sqrt(1-C_T))))).*op_D; % [1] Eq. 7.3
+    (sqrt(2)*(alpha*op_I+beta*(1-sqrt(1-C_T))))).*op_D;
 
 %% Calc k_z and k_y based on I (I=0 under ideal conditions)
 k_a = 0.38371;
 k_b = 0.003678;
-k_y = k_a*op_I + k_b; %[2] Eq.8
+
+%[2] Eq.8
+k_y = k_a*op_I + k_b;
 k_z = k_y;
 
 %% Get field width y
-sig_y = k_y.*(op_dw-x_0)+cos(yaw).*op_D/sqrt(8); % [1] Eq. 7.2
+% [1] Eq. 7.2
+sig_y = k_y.*(op_dw-x_0)+cos(yaw).*op_D/sqrt(8);
 
 %% Get field width z
-sig_z = k_z.*(op_dw-x_0)+op_D/sqrt(8); % [1] Eq. 7.2
+% [1] Eq. 7.2
+sig_z = k_z.*(op_dw-x_0)+op_D/sqrt(8);
 
 %% Calc Theta
-Theta = 0.3*yaw./cos(yaw).*(1-sqrt(1-C_T.*cos(yaw))); %[1] Eq. 6.12
+%[1] Eq. 6.12
+Theta = 0.3*yaw./cos(yaw).*(1-sqrt(1-C_T.*cos(yaw)));
 end
